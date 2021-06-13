4a187afa2c6382ab05b168bcb08726b92e8099b4
gabrielpassarinho <gabrielpassarelli@hotmail.com>
2021-06-13 18:12:14 -0300

    edicoes no makefile e na main

--- main.c
+++ main.c
@@ -4,10 +4,11 @@
 #include <math.h>
 #include <time.h>
 #include <stdlib.h>
-#define total_entradas 5
+#define total_entradas 1
 
 int main(void){
 	/*Declarando variáveis*/
+	lista* L = malloc(sizeof(lista));
 	lista* L1 = malloc(sizeof(lista));
 	lista* L2 = malloc(sizeof(lista));
 	lista* L3 = malloc(sizeof(lista));
@@ -22,6 +23,7 @@ int main(void){
 	clock_t media_tempos_rs[total_entradas];
 	
 	/*Inicia a Lista*/
+	cria(L, (long) pow(10, total_entradas));
 	cria(L1, (long) pow(10, total_entradas));
 	cria(L2, (long) pow(10, total_entradas));
 	cria(L3, (long) pow(10, total_entradas));
@@ -32,11 +34,8 @@ int main(void){
 
 		for(i=0;i<n;i++){
 			scanf("%d", &aux);
-			insere(L1, aux);
+			insere(L, aux);
 		}
-		copia(L1, L2);
-		copia(L1, L3);
-		copia(L1, L4);
 		
 		//printf("Antes do sort\n")
 		//imprime(L);
@@ -45,25 +44,38 @@ int main(void){
 		media_tempos_qs[k] = 0;
 		media_tempos_rs[k] = 0;
 		for(j = 1;  j <= 10; j++){
+			copia(L, L1);
+			copia(L, L2);
+			copia(L, L3);
+			copia(L, L4);
+			
+			//imprime(L1);
 			tempo_ini = clock();
 			/*Código que deve ser medido o tempo*/
 			ordena_heap_sort(L1);
 			media_tempos_hs[k] += clock() - tempo_ini;
+			//imprime(L1);
 			
+			//imprime(L2);
 			tempo_ini = clock();
 			/*Código que deve ser medido o tempo*/
 			ordena_bubble_sort(L2);
 			media_tempos_bs[k] += clock() - tempo_ini;
+			//imprime(L2);
 			
+			//imprime(L3);
 			tempo_ini = clock();
 			/*Código que deve ser medido o tempo*/
 			ordena_quick_sort(L3);
 			media_tempos_qs[k] += clock() - tempo_ini;
+			//imprime(L3);
 			
+			//imprime(L4);
 			tempo_ini = clock();
 			/*Código que deve ser medido o tempo*/
 			ordena_radix_sort(L4);
 			media_tempos_rs[k] += clock() - tempo_ini;
+			//imprime(L4);
 		}
 		
 		media_tempos_hs[k] = media_tempos_hs[k]/10; 
@@ -72,11 +84,6 @@ int main(void){
 		media_tempos_rs[k] = media_tempos_rs[k]/10;
 		
 		printf("Terminei uma rodada\n");
-		
-		limpa(L1);
-		limpa(L2);
-		limpa(L3);
-		limpa(L4);
 	}
 
 	printf("\nTempo total de cada\n");
