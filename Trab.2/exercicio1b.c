31cdfa810fbf2b1a3f7e6996e49f2f111a146597
gabrielpassarinho <56397335+gabrielpassarinho@users.noreply.github.com>
2021-07-31 23:03:39 -0300

    Update exercicio1b.c

--- Trab.2/exercicio1b.c
+++ Trab.2/exercicio1b.c
@@ -1,3 +1,8 @@
+/* Autores (a partir do codigo fornecido)
+Danillo Mendes Santiago 10414592
+Gabriel Passarelli 11218480
+Marcelo Kenji Noda 11275359
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -36,6 +41,41 @@ double finaliza_tempo()
     return ((double) (_fim - _ini)) / CLOCKS_PER_SEC;
 }
 
+#include "lista_int.h"
+
+int buscar(lista_int* l, int e){
+    int iteracoes = 0;
+    No* aux_pai = NULL;
+    No* aux = l->primeiro;
+
+    // busca e realoca para a 1ª posicao
+    while (aux->proximo != NULL){// itera na lista:
+        if(aux->valor == e){ // se achou
+            if(iteracoes == 0){
+                return 1;// se for o primeiro item, nao faz nada
+            }// senao: 
+
+            aux_pai->proximo = aux->proximo;// liga pai--->prox (pode ser null)
+            
+            // e coloca-o na 1ª pos da lista (existem pelo menos 2 itens na lista, facilita)
+            No* primeiro = l->primeiro;
+            l->primeiro = aux;
+            aux->proximo = primeiro;
+            
+            return 1;
+            // excecoes??****
+
+        }
+        // caso ainda nao achou...
+        // saber de onde veio e continuar loop
+        aux_pai = aux;
+        aux = aux->proximo;
+        iteracoes++;
+    }
+    return 0;//nao achou
+    
+}
+
 int main(int argc, char const *argv[])
 {
     const int N = 50000;
@@ -44,10 +84,19 @@ int main(int argc, char const *argv[])
     int* entradas = ler_inteiros("inteiros_entrada.txt", N);
     int* consultas = ler_inteiros("inteiros_busca.txt", N);
 
-    // realiza busca sequencia com realocação
+    lista_int *l;
+    cria_lista(l);
+
+    // preenche a lista com os numeros de [int* entradas]
+    for (int i = 0; i < N; i++){
+        insere_lista(l,entradas[i]);
+    }
+    
+    // realiza busca sequencial com realocação
     inicia_tempo();
     for (int i = 0; i < N; i++) {
         // buscar o elemento consultas[i] na entrada
+        encontrados += buscar(l, consultas[i]);
     }
     double tempo_busca = finaliza_tempo();
 
